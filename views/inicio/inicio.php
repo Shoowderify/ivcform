c8a902e82b1eb11654bc1580b870361980ae44d4
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2019-09-29 20:46:39 -0300

    Add files via upload

---
+++ views/inicio/inicio.php
@@ -0,0 +1,21 @@
+<h1>OBJETIVO</h1>
+               Aproximadamente en la faena se utilizan alrededor de 6.000 hojas al mes solo en operaciones, sin contabilizar el área administrativa, mantención y prevención. Es indispensable la importancia y compromiso no solo de la empresa, sino de los trabajadores, con el uso responsable que le damos a este recurso tan valioso. A continuación detallaré, algunos de los objetivos y de la importancia de la digitalización de los procesos de esta faena:   
+
+
+
+Disminución del uso de papel, por ende disminución de contaminantes al medio ambiente.
+Información instantánea, de todos los documentos que se realizan en
+        el día. Ejemplo: Report, check list, fallas en los equipos, planillas de combustible, entre otros.
+
+Más dinámico y rápido el llenado de documentos.
+Disminución de trabajo administrativo. Jefes de turno,
+        Prevencionista, departamento técnico, mantenedores etc.
+
+Disminución del costo de órdenes de solicitud de papel y costo de éste.
+Apoyo a los trabajadores en facilitar el llenado de documentos, procedimientos en linea, mapas de ubicaciones regularmente visitadas.
+Control de los trabajadores en la fecha y hora de realización de cada documento.
+Innovación al traer las nuevas forma de comunicarnos al trabajo diario.
+Icv Faena Andina se adapta a las nuevas tecnologías de la comunicación.
+
+
+EL CUIDADO DEL MEDIO AMBIENTE ES UNA TAREA DE TODOS...
