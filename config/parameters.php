4addc84c3a8f178b44f9c97bc074fbbe19d96c3b
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2019-09-29 18:50:40 -0300

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
-define("base_url", "https://apiformaciongeneral.herokuapp.com/");
+define("base_url", "https://icvform.azurewebsites.net");
 define("controller_default", "usuarioController");
 define("action_default","registro");
