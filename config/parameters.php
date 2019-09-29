7df983a8a6c048d6178ec69b253bd82959e64095
Shoowderify <38634522+Shoowderify@users.noreply.github.com>
2019-09-29 20:46:14 -0300

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
 define("base_url", "https://icvform.azurewebsites.net/");
-define("controller_default", "usuarioController");
-define("action_default","registro");
+define("controller_default", "InicioController");
+define("action_default","index");
